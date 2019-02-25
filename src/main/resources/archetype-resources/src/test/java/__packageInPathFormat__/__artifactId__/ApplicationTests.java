fe5c280ef94c3c7432aafd567a8077e13e927758
Iakov Ivanov <iackovichwat@gmail.com>
2019-02-25 16:48:23 +0300

    test: Migrate to JUnit 5

--- src/main/resources/archetype-resources/src/test/java/__packageInPathFormat__/__artifactId__/ApplicationTests.java
+++ src/main/resources/archetype-resources/src/test/java/__packageInPathFormat__/__artifactId__/ApplicationTests.java
@@ -1,17 +1,14 @@
 package ${package}.${artifactId};
 
 import ${package}.${artifactId}.web.controller.HomeController;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(HomeController.class)
 public class ApplicationTests {
 
